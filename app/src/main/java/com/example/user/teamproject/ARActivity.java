5c46ab4352b4207ef0dc8cb4452ce17654b2fbe7
donghao su <dsu1@berkeley.edu>
2018-11-06 13:16:44 -0800

    compass reading DONE

--- app/src/main/java/com/example/user/teamproject/ARActivity.java
+++ app/src/main/java/com/example/user/teamproject/ARActivity.java
@@ -1,7 +1,12 @@
 package com.example.user.teamproject;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
@@ -12,7 +17,9 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.animation.RotateAnimation;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -26,12 +33,16 @@ import java.io.IOException;
  * Created by Jason on 10/26/18.
  */
 
-public class ARActivity extends AppCompatActivity {
+public class ARActivity extends AppCompatActivity implements SensorEventListener {
     SurfaceView cameraView;
     TextView textView;
     CameraSource cameraSource;
     final int REQUEST_CAMERA_PERMISSION_ID = 1001;
 
+    private static SensorManager sensorManager;
+    private  Sensor sensor;
+
+    private  float curDegree = 0f;
     public void OnRequestPermissionsResultCallback(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResult) {
         switch (requestCode) {
             case REQUEST_CAMERA_PERMISSION_ID:
@@ -100,7 +111,7 @@ public class ARActivity extends AppCompatActivity {
 
                 @Override
                 public void receiveDetections(Detector.Detections<TextBlock> detections) {
-                    textView.setText("I saw it!!!!");
+                    //textView.setText("I saw it!!!!");
 //                    final SparseArray<TextBlock> item1 = detections.getDetectedItems();
 //                    if (item1.size() != 0) {
 //                        boolean post = textView.post(new Runnable() {
@@ -120,5 +131,63 @@ public class ARActivity extends AppCompatActivity {
                 }
             });
         }
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (sensor != null) {
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        } else {
+            Toast.makeText(this, "Not supported", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int degree = Math.round(event.values[0]);
+        String direciton = "";
+        if (degree <= 20 || degree > 335) {
+            direciton = "N";
+        } else if (degree > 20 && degree <= 65) {
+            direciton = "NE";
+        } else if (degree > 65 && degree <= 110) {
+            direciton = "E";
+        } else if (degree > 110 && degree <= 155) {
+            direciton = "SE";
+        } else if (degree > 155 && degree <= 200) {
+            direciton = "S";
+        } else if (degree > 200 && degree <= 245) {
+            direciton = "SW";
+        } else if (degree > 245 && degree <= 290) {
+            direciton = "W";
+        } else if (degree > 290 && degree <= 335) {
+            direciton = "NW";
+        } else {
+            direciton = "";
+        }
+        textView.setText(Integer.toString(degree) + (char) 0x00B0 + "  " + direciton);
+        curDegree = -degree;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
